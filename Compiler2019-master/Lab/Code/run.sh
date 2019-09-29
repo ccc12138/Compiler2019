e97486f3525635154c95f109fb60309c27dc61e8
LaJiOJ <1184264181@qq.com>
2019-09-29 13:03:49 +0800

    -m

--- Compiler2019-master/Lab/Code/run.sh
+++ Compiler2019-master/Lab/Code/run.sh
@@ -1,7 +1,7 @@
 bison -d syntax.y
 flex lexical.l
 gcc main.c syntax.tab.c syntaxTree.c syntaxTree.h -lfl -ly -o parser
-#g++ main.c syntax.tab.c syntaxTree.cpp syntaxTree.h -lfl -ly -o parser
+# g++ main.c syntax.tab.c syntaxTree.cpp syntaxTree.h -lfl -ly -o parser
 mv parser ../
 cd ../
 ./parser ./Test/test1.cmm
