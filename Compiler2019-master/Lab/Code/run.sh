8018b1f9da9d7ffe281570307850c704613356a8
LaJiOJ <1184264181@qq.com>
2019-10-11 17:11:57 +0800

    test

--- Compiler2019-master/Lab/Code/run.sh
+++ Compiler2019-master/Lab/Code/run.sh
@@ -4,7 +4,7 @@ gcc main.c syntax.tab.c syntaxTree.c syntaxTree.h -lfl -ly -o parser
 # g++ main.c syntax.tab.c syntaxTree.cpp syntaxTree.h -lfl -ly -o parser
 mv parser ../
 cd ../
-echo "------------Test1------------"
-./parser ./Test/test1.cmm
-echo "------------Test2------------"
-./parser ./Test/test2.cmm
+for((i=1;i<=25;i++)){
+	echo "------------Test $i------------"
+	./parser ./Test/test$i.cmm
+}
